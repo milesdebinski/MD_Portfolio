bcf3c3f0aa2aff046f37760d5eaae69299a3126f
Miles Debinski <miles.debinski@gmail.com>
2021-05-31 22:03:11 +0100

    add neon effect on header

--- script.js
+++ script.js
@@ -8,7 +8,47 @@ function parallax(el) {
     const y = (window.innerHeight - el.pageY * speed) / 100;
 
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    console.log([x, y]);
+    // console.log([x, y]);
   });
 }
 // ---
+// Neon effect - Header
+
+const neons_MD = document.querySelectorAll("#neon");
+const neons_FWD = document.querySelectorAll("#neon2");
+console.log(neons_MD);
+
+const turnOnNeons_MD = () => {
+  neons_MD.forEach((el, i) => {
+    setTimeout(() => {
+      el.classList.add("show");
+    }, i * 400);
+
+    setTimeout(() => {
+      setTimeout(() => {
+        el.classList.remove("show");
+      }, i * 400);
+    }, 5000);
+  });
+};
+
+const turnOnNeons_FWD = () => {
+  neons_FWD.forEach((el, i) => {
+    setTimeout(() => {
+      el.classList.add("show");
+    }, i * 240);
+
+    setTimeout(() => {
+      setTimeout(() => {
+        el.classList.remove("show");
+      }, i * 240);
+    }, 5000);
+  });
+};
+
+setInterval(() => {
+  turnOnNeons_MD();
+  turnOnNeons_FWD();
+}, 13000);
+turnOnNeons_MD();
+turnOnNeons_FWD();
