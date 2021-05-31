cbeca56248dd9a3bc40176b082540f0c9bd5484f
Miles Debinski <miles.debinski@gmail.com>
2021-05-31 18:02:14 +0100

    add parallax

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+// Header - Parallax Effect
+document.addEventListener("mousemove", parallax);
+function parallax(el) {
+  this.querySelectorAll(".layer").forEach((layer) => {
+    const speed = layer.getAttribute("data-speed");
 
+    const x = (window.innerWidth - el.pageX * speed) / 300;
+    const y = (window.innerHeight - el.pageY * speed) / 100;
 
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    console.log([x, y]);
+  });
+}
+// ---
